b2b094424e44ed3b33fe93a39995382a5a37eb51
wangyao <wangyao@cis.com.cn>
2019-07-14 20:42:35 +0800

    项目初始化

---
+++ src/test/java/com/mohan/project/easycache/EasyCacheApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mohan.project.easycache;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EasyCacheApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
